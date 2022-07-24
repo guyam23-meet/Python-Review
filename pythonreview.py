ea2110986a0b5ff447b32782b67ed87aa9ccc6d3
guyam23-meet <guyamibar@gmail.com>
2022-07-24 23:41:04 +0300

    i did the bonus yay

--- pythonreview.py
+++ pythonreview.py
@@ -1,5 +1,5 @@
-def create_youtube_video(title,description):
-	vid_dict = {"title":title,"description":description,"likes":0,"dislikes":0,"comments":{}}
+def create_youtube_video(title,description,hashtag):
+	vid_dict = {"title":title,"description":description,"likes":0,"dislikes":0,"comments":{},"hastags":hashtag}
 	return vid_dict
 def likes(video):
 	if "likes" in video:
@@ -14,6 +14,19 @@ def dislikes(video):
 def add_comment(youtubevideo,username,comment_text):
 	youtubevideo["comments"][username]=comment_text
 	return youtubevideo
+
+def similarity_to_video(vid1,vid2):
+	if vid1["hashtags"].len()>vid2["hasgtags"].len():
+		sml = vid2["hashtags"]
+		big = vid1["hashtags"]
+	else:
+		sml = vid1["hashtags"]
+		big = vid2["hashtags"]
+	count=0
+	for i in range(sml.len()):
+		if sml[i] in big:
+			count++
+	return string(count/sml.len()*100)+"%"
 
 IPrankedMyGF = create_youtube_video("OMG guys i pranked my girlfriend at 3am???","so yesterday at 3am me and the boys took my GF threw her in the pull drownd her and hid her body. best prank ever!")
 
